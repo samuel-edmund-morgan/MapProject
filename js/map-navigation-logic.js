0f725083e0a5fc407dc36a115d20329d20ab346b
samuel-edmund-morgan <samuel.edmund.morgan@nasbu.edu.ua>
2024-11-08 14:32:10 +0200

    Changed styles

--- js/map-navigation-logic.js
+++ js/map-navigation-logic.js
@@ -1,4 +1,64 @@
+// noinspection TypeScriptUMDGlobal
+
 let highlightLayer;
+
+const map = L.map('map', {
+    zoomControl: false,
+    maxZoom: 8,
+    minZoom: 6,
+    tileSize: 512,
+    keepBuffer: 2,
+    preferCanvas: true
+});
+
+// function highlightFeature(e) {
+//     // Reset the previously highlighted region if it exists
+//     if (highlightLayer) {
+//         highlightLayer.setStyle({
+//             color: 'rgba(255,223,0,1.0)',  // Original border color
+//             fillColor: 'rgba(0,87,184,1.0)',  // Original fill color
+//             fillOpacity: 1  // Original fill opacity
+//         });
+//     }
+//     highlightLayer = e.target;  // Set the new highlight layer
+//
+//     // Apply the highlight style to the clicked region
+//     highlightLayer.setStyle({
+//         color: 'rgba(255,223,0,1.0)',  // Highlight border color
+//         fillColor: '#008000',  // Highlight fill color
+//         fillOpacity: 1  // Highlight fill opacity
+//     });
+//
+//     // Center and zoom the map on the clicked feature
+//     const bounds = highlightLayer.getBounds();
+//     map.flyToBounds(bounds, {
+//         maxZoom: 8,  // Adjust maxZoom as needed
+//         animate: true
+//     });
+// }
+
+
+function highlightOnHoverFeature(e) {
+    // Only change style if the hovered layer is not the currently highlighted (clicked) layer
+    if (e.target !== highlightLayer) {
+        e.target.setStyle({
+            color: 'rgba(255,223,0,1.0)',  // Border color on hover
+            fillColor: '#008000',  // Fill color on hover
+            fillOpacity: 0.85
+        });
+    }
+}
+
+function resetHoverFeature(e) {
+    // Only reset style if it's not the currently highlighted (clicked) layer
+    if (e.target !== highlightLayer) {
+        e.target.setStyle({
+            color: 'rgba(255,223,0,1.0)',  // Original border color
+            fillColor: 'rgba(0,87,184,1.0)',  // Original fill color
+            fillOpacity: 1  // Original fill opacity
+        });
+    }
+}
 
 function highlightFeature(e) {
     // Reset the previously highlighted region if it exists
@@ -9,29 +69,68 @@ function highlightFeature(e) {
             fillOpacity: 1  // Original fill opacity
         });
     }
-    highlightLayer = e.target;  // Set the new highlight layer
-
-    // Apply the highlight style to the clicked region
+    // Set the clicked region as the highlighted layer
+    highlightLayer = e.target;
     highlightLayer.setStyle({
         color: 'rgba(255,223,0,1.0)',  // Highlight border color
-        fillColor: '#008000',  // Highlight fill color
+        fillColor: '#008000',  // Highlight fill color on click
         fillOpacity: 1  // Highlight fill opacity
     });
 
     // Center and zoom the map on the clicked feature
-    var bounds = highlightLayer.getBounds();
+    const bounds = highlightLayer.getBounds();
     map.flyToBounds(bounds, {
         maxZoom: 8,  // Adjust maxZoom as needed
         animate: true
     });
+
 }
 
+
+//
+// function highlightOnHoverFeature(e) {
+//     // Reset the previously highlighted region if it exists
+//     if (highlightLayer) {
+//
+//         highlightLayer.setStyle({
+//             color: 'rgba(255,223,0,1.0)',  // Original border color
+//             fillColor: 'rgba(0,87,184,1.0)',  // Original fill color
+//             fillOpacity: 1  // Original fill opacity
+//         });
+//     }
+//     highlightLayer = e.target;  // Set the new highlight layer
+//     // Apply the highlight style to the clicked region
+//     highlightLayer.setStyle({
+//         color: 'rgba(255,223,0,1.0)',  // Highlight border color
+//         fillColor: '#008000',  // Highlight fill color
+//         fillOpacity: 1  // Highlight fill opacity
+//     });
+// }
+
+
+
+
+
+
+
+
+
+const layer_ukr_admbnda_adm1_sspe_20240416_0 = new L.geoJson(json_ukr_admbnda_adm1_sspe_20240416_0, {
+    attribution: '',
+    interactive: true,
+    dataVar: 'json_ukr_admbnda_adm1_sspe_20240416_0',
+    layerName: 'layer_ukr_admbnda_adm1_sspe_20240416_0',
+    pane: 'pane_ukr_admbnda_adm1_sspe_20240416_0',
+    onEachFeature: pop_ukr_admbnda_adm1_sspe_20240416_0,
+    style: style_ukr_admbnda_adm1_sspe_20240416_0_0,
+});
+
 function populateRegionList() {
-    var regionList = document.getElementById('regions');
+    const regionList = document.getElementById('regions');
     layer_ukr_admbnda_adm1_sspe_20240416_0.eachLayer(function(layer) {
-        var regionName = layer.feature.properties['SSU'];
+        const regionName = layer.feature.properties['SSU'];
         if (regionName) {
-            var listItem = document.createElement('li');
+            const listItem = document.createElement('li');
             listItem.textContent = regionName;
             listItem.style.cursor = 'pointer';
             listItem.onclick = function() {
@@ -42,48 +141,13 @@ function populateRegionList() {
     });
 }
 
-var map = L.map('map', {
-    zoomControl: false,
-    maxZoom: 8,
-    minZoom: 6
-});
-//var hash = new L.Hash(map);
-
-//var autolinker = new Autolinker({truncate: {length: 30, location: 'smart'}});
-// remove popup's row if "visible-with-data"
-// function removeEmptyRowsFromPopupContent(content, feature) {
-//     var tempDiv = document.createElement('div');
-//     tempDiv.innerHTML = content;
-//     var rows = tempDiv.querySelectorAll('tr');
-//     for (var i = 0; i < rows.length; i++) {
-//         var td = rows[i].querySelector('td.visible-with-data');
-//         var key = td ? td.id : '';
-//         if (td && td.classList.contains('visible-with-data') && feature.properties[key] == null) {
-//             rows[i].parentNode.removeChild(rows[i]);
-//         }
-//     }
-//     return tempDiv.innerHTML;
-// }
-// add class to format popup if it contains media
-// function addClassToPopupIfMedia(content, popup) {
-//     var tempDiv = document.createElement('div');
-//     tempDiv.innerHTML = content;
-//     if (tempDiv.querySelector('td img')) {
-//         popup._contentNode.classList.add('media');
-//         // Delay to force the redraw
-//         setTimeout(function() {
-//             popup.update();
-//         }, 10);
-//     } else {
-//         popup._contentNode.classList.remove('media');
-//     }
-// }
-var zoomControl = L.control.zoom({
+const zoomControl = L.control.zoom({
     position: 'topleft'
-})
+});
 
 zoomControl.addTo(map);
-var bounds_group = new L.featureGroup([]);
+const bounds_group = new L.featureGroup([]);
+
 function setBounds() {
     if (bounds_group.getLayers().length) {
         map.fitBounds(bounds_group.getBounds());
@@ -95,6 +159,8 @@ function setBounds() {
 function pop_ukr_admbnda_adm1_sspe_20240416_0(feature, layer) {
     layer.on({
         click: highlightFeature,
+        mouseover: highlightOnHoverFeature,
+        mouseout: resetHoverFeature
     });
 }
 
@@ -116,20 +182,11 @@ function style_ukr_admbnda_adm1_sspe_20240416_0_0() {
 map.createPane('pane_ukr_admbnda_adm1_sspe_20240416_0');
 map.getPane('pane_ukr_admbnda_adm1_sspe_20240416_0').style.zIndex = 400;
 map.getPane('pane_ukr_admbnda_adm1_sspe_20240416_0').style['mix-blend-mode'] = 'normal';
-var layer_ukr_admbnda_adm1_sspe_20240416_0 = new L.geoJson(json_ukr_admbnda_adm1_sspe_20240416_0, {
-    attribution: '',
-    interactive: true,
-    dataVar: 'json_ukr_admbnda_adm1_sspe_20240416_0',
-    layerName: 'layer_ukr_admbnda_adm1_sspe_20240416_0',
-    pane: 'pane_ukr_admbnda_adm1_sspe_20240416_0',
-    onEachFeature: pop_ukr_admbnda_adm1_sspe_20240416_0,
-    style: style_ukr_admbnda_adm1_sspe_20240416_0_0,
-});
 bounds_group.addLayer(layer_ukr_admbnda_adm1_sspe_20240416_0);
 map.addLayer(layer_ukr_admbnda_adm1_sspe_20240416_0);
 
 function updateTooltips() {
-    var currentZoom = map.getZoom();
+    const currentZoom = map.getZoom();
     layer_ukr_admbnda_adm1_sspe_20240416_0.eachLayer(function(layer) {
         if (currentZoom === 8) {
             // Bind tooltip only at zoom level 8
@@ -155,21 +212,10 @@ function updateTooltips() {
         }
     });
 }
-
-
 setBounds();
 populateRegionList();
 
-
-
-
-var i = 0;
 layer_ukr_admbnda_adm1_sspe_20240416_0.eachLayer(function(layer) {
-    // var context = {
-    //     feature: layer.feature,
-    //     variables: {}
-    // };
-
     layer.bindTooltip(
         (layer.feature.properties['ADM1_UA'] !== null
             ? String('<div style="color: #ffffff; font-size: 10pt; font-weight: bold; font-family: \'Montserrat\', sans-serif; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">' + layer.feature.properties['ADM1_UA']) + '</div>'
@@ -181,23 +227,11 @@ layer_ukr_admbnda_adm1_sspe_20240416_0.eachLayer(function(layer) {
             className: 'css_ukr_admbnda_adm1_sspe_20240416_0'
         }
     );
-
-    // labels.push(layer);
-    // totalMarkers += 1;
-    // layer.added = true;
-    // addLabel(layer, i);
-    // i++;
 });
 resetLabels([layer_ukr_admbnda_adm1_sspe_20240416_0]);
-
-
-
 map.on("zoomend", updateTooltips);
 // Initial call to set the tooltip visibility on page load
 updateTooltips();
-// map.on("zoomend", function(){
-//     resetLabels([layer_ukr_admbnda_adm1_sspe_20240416_0]);
-// });
 map.on("layeradd", function(){
     resetLabels([layer_ukr_admbnda_adm1_sspe_20240416_0]);
 });
